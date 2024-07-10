c0732a2fcbea4c828cb12fbd9e6a065062eeefed
sahil malpotra <106588780+sahilmalpotra@users.noreply.github.com>
2024-07-11 01:22:02 +0530

    change startconatiner

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker push kubesahilmalpotra/simple-flask-app
+docker pull kubesahilmalpotra/simple-flask-app
 
 # Run the Docker image as a container
 docker run -d -p 5000:5000 docker push kubesahilmalpotra/simple-flask-app
